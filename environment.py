50902540f5ba70edb6cf3f4f7cd95a4fd0892720
PhilippThoelke <p.thoelke@gmx.de>
2020-01-27 11:24:00 +0100

    Change matplotlib backend to avoid figure close error

--- environment.py
+++ environment.py
@@ -1,6 +1,10 @@
 import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
 import gc
+
+# default backend causes Tcl_AsyncDelete error
+matplotlib.use('Tkagg')
 
 class Environment:
 
