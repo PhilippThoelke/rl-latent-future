d75132e4f8a0f513bcb61b662b4942d2c637580e
PhilippThoelke <p.thoelke@gmx.de>
2020-01-27 14:59:10 +0100

    Parallelize data preprocessing

--- simple_dqn.py
+++ simple_dqn.py
@@ -1,4 +1,5 @@
 from environment import Environment
+import tensorflow as tf
 from tensorflow.keras import models, layers, optimizers
 import numpy as np
 from datetime import datetime
@@ -6,14 +7,15 @@ import os
 from joblib import Parallel, delayed
 from matplotlib import pyplot as plt
 
-GAMMA = 0.5
+GAMMA = 0.7
 LEARNING_RATE = 0.001
-EPSILON_DECAY = 0.9
+EPSILON_DECAY = 0.975
 MIN_EPSILON = 0.03
-SIMULATION_EPOCHS = 16
-TRAINING_EPOCHS = 2
+SIMULATION_EPOCHS = 128
+TRAINING_EPOCHS = 5
 NUM_ACTIONS = 8
-SHOW_AFTER_ITERATIONS = 10
+SHOW_AFTER_ITERATIONS = -1
+BATCH_SIZE = 512
 
 def get_model():
     # return a compiled model to be used as the deep Q network
@@ -55,11 +57,20 @@ def simulate(weights, epsilon):
             observations[-1].extend([reward, state])
         if not done:
             observations.append([state, prediction, action])
-    return observations, env
 
-def plot_simulation():
+    # preprocess the observation tuples
+    for i, current in enumerate(observations):
+        action = current[2]
+        current_q = current[1][action]
+        reward = current[3]
+        next_max_q = np.max(model(current[4].reshape((1, -1))).numpy())
+        current[1][action] = reward + GAMMA * next_max_q
+
+    return [obs[0] for obs in observations], [obs[1] for obs in observations], env.cumulative_reward
+
+def plot_simulation(env, model):
     # set up the environment
-    env = Environment(draw=True)
+    env.reset()
     state = env.state()
 
     # run the simulation
@@ -70,6 +81,7 @@ def plot_simulation():
 
 if __name__ == '__main__':
     model = get_model()
+    plot_env = None
 
     # create folder for model checkpoints
     timestamp = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
@@ -78,37 +90,40 @@ if __name__ == '__main__':
     epsilon = 1
     iteration = 0
     while True:
+        tf.keras.backend.clear_session()
         iteration += 1
         print(f'Iteration {iteration}:')
 
         # run simulations
         print(f'Running {SIMULATION_EPOCHS} simulations...')
         params = model.get_weights(), epsilon
-        results = Parallel(n_jobs=-1)(delayed(simulate)(*params) for _ in range(SIMULATION_EPOCHS))
+        delayed_call = (delayed(simulate)(*params) for _ in range(SIMULATION_EPOCHS))
+        results = Parallel(n_jobs=-1)(delayed_call)
 
-        # get observations and cumulative rewards from the results list
-        observations = sum([current[0] for current in results], [])
-        mean_rewards = np.mean([current[1].cumulative_reward for current in results])
+        # extract x, y and cumulative rewards from results
+        x = np.array(sum([res[0] for res in results], []))
+        y = np.array(sum([res[1] for res in results], []))
+        mean_rewards = np.mean([res[2] for res in results])
 
         # decay the exploration rate
         epsilon *= EPSILON_DECAY
         epsilon = max(epsilon, MIN_EPSILON)
 
-        # preprocess the returned observation tuples
-        for i, current in enumerate(observations):
-            action = current[2]
-            current_q = current[1][action]
-            reward = current[3]
-            next_max_q = np.max(model(current[4].reshape((1, -1))).numpy())
-            current[1][action] = reward + GAMMA * next_max_q
-
-        # get inputs and targets from the preprocessed observations
-        x = np.array([current[0] for current in observations])
-        y = np.array([current[1] for current in observations])
-
         # train the model and save the current weights
         print(f'Training for {TRAINING_EPOCHS} epochs...')
-        model.fit(x, y, epochs=TRAINING_EPOCHS, shuffle=True, batch_size=128, verbose=0)
+        for epoch in range(TRAINING_EPOCHS):
+            # shuffle training data
+            permutation = np.random.permutation(len(x))
+            x = x[permutation]
+            y = y[permutation]
+
+            # train for one epoch
+            for i in range(len(x) // BATCH_SIZE + 1):
+                start = i * BATCH_SIZE
+                end = (i + 1) * BATCH_SIZE
+                model.train_on_batch(x[start:end], y[start:end])
+
+        # save the current model to disk
         model.save(os.path.join('models', timestamp, f'model_{iteration}_{int(mean_rewards)}.h5'))
 
         # evaluate the training state
@@ -116,6 +131,8 @@ if __name__ == '__main__':
         print(f'Current epsilon: {epsilon:.2f}, loss: {loss:.4f}, avg. cumulative reward: {mean_rewards:.2f}')
 
         # visualize the model's progress by plotting a simulation
-        if iteration % SHOW_AFTER_ITERATIONS == 0:
-            plot_simulation()
+        if SHOW_AFTER_ITERATIONS > 0 and iteration % SHOW_AFTER_ITERATIONS == 0:
+            if plot_env is None:
+                plot_env = Environment(draw=True)
+            plot_simulation(plot_env, model)
         print()
